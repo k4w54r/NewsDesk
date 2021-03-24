7a98f73cbb9d89d92707c58314a487c8363bb6ff
Kawsar <k4w54r13@gmail.com>
2021-03-24 23:31:07 +0600

    Changed designe to three columns

--- routes/article.js
+++ routes/article.js
@@ -5,12 +5,28 @@ const Article = require('../models/Article');
 
 router.get('/getArticles/', async (req, res) => {
   try {
-    let articles = await Article.find({}, { _id: 0 });
+    let articles = await Article.find();
     res.json(articles);
   } catch (err) {
     console.error(err.message);
     if (err.kind == 'ObjectId') {
       return res.status(404).json({ msg: 'Articles not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+router.get('/getArticle/:id', async (req, res) => {
+  try {
+    let article = await Article.findById(req.params.id);
+    if (!article) {
+      return res.status(404).json({ msg: 'Article not found' });
+    }
+    res.json(article);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(404).json({ msg: 'Article not found' });
     }
     res.status(500).send('Server Error');
   }
